f5bbce9201e01565e748e5bf64f97ea99afaf87d
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2021-12-03 08:43:36 +0200

    Added strings.dart

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Tawfeer',
       home: Scaffold(
         appBar: AppBar(
