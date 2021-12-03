51829939429529ef81dcdc52b17a7c6e9c38a883
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2021-12-03 09:16:47 +0200

    Added colors.dart

---
+++ lib/src/ui/themes/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color green = const Color(0xFF1F9E67);
+  static Color lightGrey = const Color(0xFFEFEFEF);
+  static Color darkGrey = const Color(0xFF565656);
+}
