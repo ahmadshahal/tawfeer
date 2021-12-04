6d9246a63d4358f93939d899982c3968bf08b21c
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2021-12-04 08:42:03 +0200

    Added validation_utility.dart

---
+++ lib/src/business_logic/utils/validation_utility.dart
@@ -0,0 +1,16 @@
+class ValidationUtility {
+  static String? loginValidateEmail(String email) {
+    if (email.length < 10 ||
+        email.substring(email.length - 10) != "@gmail.com"){
+      return "Invalid Email";
+    }
+    return null;
+  }
+
+  static String? loginValidatePassword(String password) {
+    if(password.isEmpty) {
+      return "Invalid Password";
+    }
+    return null;
+  }
+}
