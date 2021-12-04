a3791eeacf9783683072e8b38f8d9c5ef0cb9ad3
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2021-12-04 13:11:04 +0200

    Added loading_dialog.dart

---
+++ lib/src/ui/components/loading_dialog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoadingDialog extends StatelessWidget {
+  const LoadingDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
