40c7ad47c7ce651f714f29e1ff59563c559c11f8
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2021-12-05 07:23:17 +0200

    Added HomeLayout screens.

--- lib/src/ui/screens/modules/profile_screen.dart
+++ lib/src/ui/screens/modules/profile_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tawfeer/src/ui/themes/styles/colors.dart';
 
-class ProductsScreen extends StatelessWidget {
-  const ProductsScreen({Key? key}) : super(key: key);
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Center(
       child: Text(
-          'Products Screen',
+        'Profile Screen',
         style: TextStyle(
           fontSize: 17.0,
           color: MyColors.darkBlue,
--- lib/src/ui/screens/modules/search_screen.dart
+++ lib/src/ui/screens/modules/search_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tawfeer/src/ui/themes/styles/colors.dart';
 
-class ProductsScreen extends StatelessWidget {
-  const ProductsScreen({Key? key}) : super(key: key);
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Center(
       child: Text(
-          'Products Screen',
+        'Search Screen',
         style: TextStyle(
           fontSize: 17.0,
           color: MyColors.darkBlue,
