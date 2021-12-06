df44fb0959954ab8004b25f5a18b943dfb85ad66
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2021-12-06 11:18:31 +0200

    Modified login_cubit.dart and register_cubit.dart so it can fetch user data before entering the application.

--- lib/src/business_logic/bloc/cubits/login_cubit/login_cubit.dart
+++ lib/src/business_logic/bloc/cubits/login_cubit/login_cubit.dart
@@ -1,17 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:tawfeer/src/business_logic/bloc/repositories/auth_repository.dart';
+import 'package:tawfeer/src/business_logic/bloc/repositories/user_repository.dart';
+import 'package:tawfeer/src/business_logic/shared/my_user.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
   final AuthRepository _authRepository = AuthRepository();
+  final UserRepository _userRepository = UserRepository();
 
   Future<void> submit({required String email, required String password}) async {
     emit(LoginSubmitting());
     try {
+      // TODO: should return id or token.
       await _authRepository.login(email: email, password: password);
+      MyUser.myUser = await _userRepository.fetchUser(id: 1);
       emit(LoginSuccess());
     } catch (ex) {
       emit(LoginFailure(exception: ex as Exception));
--- lib/src/business_logic/bloc/cubits/register_cubit/register_cubit.dart
+++ lib/src/business_logic/bloc/cubits/register_cubit/register_cubit.dart
@@ -1,12 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:tawfeer/src/business_logic/bloc/repositories/auth_repository.dart';
+import 'package:tawfeer/src/business_logic/bloc/repositories/user_repository.dart';
+import 'package:tawfeer/src/business_logic/shared/my_user.dart';
 
 part 'register_state.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
   RegisterCubit() : super(RegisterInitial());
   final AuthRepository _authRepository = AuthRepository();
+  final UserRepository _userRepository = UserRepository();
 
   Future<void> submit({
     required String fullName,
@@ -16,7 +19,9 @@ class RegisterCubit extends Cubit<RegisterState> {
   }) async {
     emit(RegisterSubmitting());
     try {
+      // TODO: should return id or token.
       await _authRepository.register(fullName: fullName, email: email, password: password, phoneNumber: phoneNumber);
+      MyUser.myUser = await _userRepository.fetchUser(id: 1);
       emit(RegisterSuccess());
     } catch (ex) {
       emit(RegisterFailure(exception: ex as Exception));
