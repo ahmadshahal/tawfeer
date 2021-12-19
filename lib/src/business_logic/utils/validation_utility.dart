1e271c682b21a8ec9f48f891cb61f07bebdabe98
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2021-12-19 09:31:41 +0200

    Updated validation_utility.dart

--- lib/src/business_logic/utils/validation_utility.dart
+++ lib/src/business_logic/utils/validation_utility.dart
@@ -36,11 +36,43 @@ class ValidationUtility {
     return null;
   }
 
+  static String? validateProductTitle(String title) {
+    if(title.isEmpty) {
+      return "Title can't be empty";
+    }
+    return null;
+  }
+
+  static String? validateProductCategory(String category) {
+    if(category.isEmpty) {
+      return "Category can't be empty";
+    }
+    return null;
+  }
+
   static String? validateRegisterPhoneNumber(String phoneNumber) {
-    if(phoneNumber.length != 9) {
+    if(phoneNumber.length != 9 || !ValidationUtility.isNumeric(phoneNumber)) {
       return "Invalid phone number";
     }
     return null;
+  }
+
+  static String? validateNumericField(String s) {
+    if(s.isEmpty || !ValidationUtility.isNumeric(s)) {
+      return "Invalid value";
+    }
+    return null;
+  }
+
+  static String? validateDate(String s) {
+    if(s.isEmpty) {
+      return "Date can't be empty";
+    }
+    return null;
+  }
+
+  static bool isNumeric(String s) {
+    return double.tryParse(s) != null;
   }
 
 }
