c0b48b7ee9dea3471cbb40bc41f73eedb5faa4dd
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2021-12-22 17:27:07 +0200

    Updated search_screen and added search.png.

--- lib/src/ui/utils/custom_search_delegate.dart
+++ lib/src/ui/utils/custom_search_delegate.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tawfeer/src/business_logic/models/product.dart';
 import 'package:tawfeer/src/ui/components/my_list_view.dart';
+import 'package:tawfeer/src/ui/utils/non_glow_scroll_behavior.dart';
 
 class CustomSearchDelegate extends SearchDelegate {
   final List<Product> products;
@@ -35,15 +36,24 @@ class CustomSearchDelegate extends SearchDelegate {
   Widget buildResults(BuildContext context) {
     List<Product> matchQuery = [];
     for (Product product in products) {
-      if (product.category.toLowerCase().contains(query.toLowerCase()) ||
-          product.productTitle.toLowerCase().contains(query.toLowerCase()) ||
-          product.newPrice
-              .toString()
-              .toLowerCase()
-              .contains(query.toLowerCase()) ||
-          product.description.toLowerCase().contains(query.toLowerCase())) {
+      // TODO: Add Date search condition.
+      if (query.isNotEmpty &&
+          (product.category.toLowerCase().contains(query.toLowerCase()) ||
+              product.productTitle
+                  .toLowerCase()
+                  .contains(query.toLowerCase()) ||
+              product.newPrice
+                  .toString()
+                  .toLowerCase()
+                  .contains(query.toLowerCase()) ||
+              product.description
+                  .toLowerCase()
+                  .contains(query.toLowerCase()))) {
         matchQuery.add(product);
       }
+    }
+    if (matchQuery.isEmpty) {
+      return _userMsg(context);
     }
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -71,15 +81,24 @@ class CustomSearchDelegate extends SearchDelegate {
   Widget buildSuggestions(BuildContext context) {
     List<Product> matchQuery = [];
     for (Product product in products) {
-      if (product.category.toLowerCase().contains(query.toLowerCase()) ||
-          product.productTitle.toLowerCase().contains(query.toLowerCase()) ||
-          product.newPrice
-              .toString()
-              .toLowerCase()
-              .contains(query.toLowerCase()) ||
-          product.description.toLowerCase().contains(query.toLowerCase())) {
+      // TODO: Add Date search condition.
+      if (query.isNotEmpty &&
+          (product.category.toLowerCase().contains(query.toLowerCase()) ||
+              product.productTitle
+                  .toLowerCase()
+                  .contains(query.toLowerCase()) ||
+              product.newPrice
+                  .toString()
+                  .toLowerCase()
+                  .contains(query.toLowerCase()) ||
+              product.description
+                  .toLowerCase()
+                  .contains(query.toLowerCase()))) {
         matchQuery.add(product);
       }
+    }
+    if (matchQuery.isEmpty) {
+      return _userMsg(context);
     }
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -97,6 +116,30 @@ class CustomSearchDelegate extends SearchDelegate {
               MyListView(list: matchQuery),
               const SizedBox(height: 15.0),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _userMsg(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0, left: 16.0, bottom: 32.0),
+      child: Center(
+        child: ScrollConfiguration(
+          behavior: NonGlowScrollBehavior(),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/search.png',
+                  height: 350,
+                  width: 350,
+                ),
+                const Text('No Results..'),
+              ],
+            ),
           ),
         ),
       ),
